6d04b22a7f3af827d0042f5a76eb7b2114052feb
CalWah <160150308+CalWah@users.noreply.github.com>
2024-02-15 16:49:10 +0000

    Homework example

---
+++ datatype_conversion.py
@@ -0,0 +1,30 @@
+'''
+#covering datatype
+
+weekly_rate = input("Enter your wekly pay: ")
+
+weekly_rate = float(weekly_rate)
+
+print("Your weekly pay is", weekly_rate)
+
+#See what the datatype is for a variable
+print(type(weekly_rate))
+
+#Display bi-weekly pay
+print("Every two weeks you make", "$" + str(weekly_rate * 2), "dollars")
+
+'''
+
+
+
+#In your hw, do not hardcode. Get vaules from user. Remember to convert to a datatype
+
+num1 = 3
+num2 = 5
+
+
+print(num1, "*", num2, "=", num1 * num2)
+
+#Using exponets
+
+print(num1, "raised to the power of", num2, "is", num1**num2)
